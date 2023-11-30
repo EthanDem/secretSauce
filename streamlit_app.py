1a8b1d86b29189a091680f29d1c8742017f7ef83
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-11-30 14:10:28 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import requests
 import streamlit as st
 
+st.set_page_config(page_title="Secret Sauce", page_icon=":shushing_face:")
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {
+    visibility: hidden;
+}
+footer:after {
+    content: 'made by nBrain';
+    visibility: visible;
+    display: block;
+    position: relative;
+    /* background-color: red; */
+    padding: 5px;
+    top: 2px;
+}
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 url = "https://ethantest.icecube1513.repl.co/request"
 
